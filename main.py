f142c4b9e19041d55d27d3f7c10848ad084f97ca
KeithSantamaria <keith.santamaria715@gmail.com>
2025-04-24 00:29:10 -0500

    Got a black screen to display

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 import pygame
 
+from constants import (SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, 
+ASTEROID_KINDS, ASTEROID_SPAWN_RATE, ASTEROID_MAX_RADIUS)
+
+def game_loop(screen):
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill(color="black")
+        pygame.display.flip()
+
+
 def main():
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_loop(screen)
 
 if __name__ == "__main__":
     main()
