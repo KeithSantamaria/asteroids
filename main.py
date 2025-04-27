33d605dafd80afd7c70efac3d468894ca4bd5a51
KeithSantamaria <keith.santamaria715@gmail.com>
2025-04-27 00:04:07 -0500

    Moved player into group

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ ASTEROID_KINDS, ASTEROID_SPAWN_RATE, ASTEROID_MAX_RADIUS)
 
 from player import Player
 
-def game_loop(screen, player):
+def game_loop(screen, updatable, drawable):
     clock = pygame.time.Clock()
     dt = 0
     while True:
@@ -14,8 +14,10 @@ def game_loop(screen, player):
                 return
         dt = clock.tick(60) / 1000 #pauses game loop to 1/60 second
         screen.fill(color="black")
-        player.draw(screen)
-        player.update(dt)
+        for entity in updatable:
+            entity.update(dt)
+        for entity in drawable:
+            entity.draw(screen)
         pygame.display.flip() # refreshes screen
         
         
@@ -27,11 +29,14 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+
     pygame.init()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player( SCREEN_WIDTH/2,  SCREEN_HEIGHT /2)
-    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    game_loop(screen, player)
+    game_loop(screen, updatable, drawable)
 
 if __name__ == "__main__":
     main()
