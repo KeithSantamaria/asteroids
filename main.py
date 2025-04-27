e3b5e380bb13c5a73b0c0ab673b4142c4a4528e5
KeithSantamaria <keith.santamaria715@gmail.com>
2025-04-26 23:03:53 -0500

    Able to draw player sprite

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import pygame
 from constants import (SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, 
 ASTEROID_KINDS, ASTEROID_SPAWN_RATE, ASTEROID_MAX_RADIUS)
 
-def game_loop(screen):
+from player import Player
+
+def game_loop(screen, player):
     clock = pygame.time.Clock()
     dt = 0
     while True:
@@ -11,8 +13,10 @@ def game_loop(screen):
             if event.type == pygame.QUIT:
                 return
         screen.fill(color="black")
+        player.draw(screen)
         pygame.display.flip() # refreshes screen
         dt = clock.tick(60) / 1000 #pauses game loop to 1/60 second
+        
         print(f"Delta time: {dt} seconds")
 
 
@@ -22,10 +26,10 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
-    
+    player = Player( SCREEN_WIDTH/2,  SCREEN_HEIGHT /2)
     
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    game_loop(screen)
+    game_loop(screen, player)
 
 if __name__ == "__main__":
     main()
